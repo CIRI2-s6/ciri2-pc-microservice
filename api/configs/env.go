2c8670e65805f33976995795fe3da1673e8bdbe0
Jordy Walraven <98609378+JordyWalraven@users.noreply.github.com>
2024-04-07 21:43:05 +0200

    Update env

--- api/configs/env.go
+++ api/configs/env.go
@@ -2,9 +2,17 @@ package configs
 
 import (
 	"os"
+
+	"github.com/joho/godotenv"
 )
 
 func EnvMongoURI() string {
+	println("MONGOURI: ", os.Getenv("MONGOURI"))
+	envFile, err := godotenv.Read(".env")
+	if envFile != nil && err == nil {
+		println("MONGOURI: ", envFile["MONGOURI"])
+		return envFile["MONGOURI"]
+	}
 
 	return os.Getenv("MONGOURI")
 }
